733749f22a77064811a2c13f625d3d488b0fefd6
TriiiHaus <nguytri8598@gmail.com>
2021-10-10 16:03:46 -0700

    models index.js created

--- models/index.js
+++ models/index.js
@@ -5,13 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: 'Category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'priced-item'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Products{
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tagged-products'
+})
 module.exports = {
   Product,
   Category,
